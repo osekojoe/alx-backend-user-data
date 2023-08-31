de70498eeafc1faf6fb106776afe3945498dd666
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-31 16:56:43 +0200

    encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,13 +8,10 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """
-    Salted pass generation
-    """
+    """Salted and hashed password generation"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ is valid?
-    """
+    """ is password valid?"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
