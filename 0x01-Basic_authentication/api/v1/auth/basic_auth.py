4643940f075d6596cf0b93f52c3010496a9d73ff
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-05 13:42:41 +0200

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication handler
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Authentication handler - inherits from Auth
+    """
