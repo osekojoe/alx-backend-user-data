15d83badf1979aee2fcaaf70766b373f26c66123
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-06 20:30:19 +0200

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -65,11 +65,14 @@ def before_request():
 
     # paths which dont require authentication
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     # Check if the request path is not in the excluded_paths list
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and \
+                auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
