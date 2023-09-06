3e5e1ae18eb4e4bf5f5ce8427f939bddfdb01ffe
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-06 20:05:04 +0200

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,6 +11,9 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
+from api.v1.auth.session_auth import SessionAuth
+
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -22,8 +25,10 @@ auth = None
 auth_type = os.getenv("AUTH_TYPE")
 if auth_type == 'auth':
     auth = Auth()
-elif auth_type == 'basic_auth':
+if auth_type == 'basic_auth':
     auth = BasicAuth()
+if auth_type == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
