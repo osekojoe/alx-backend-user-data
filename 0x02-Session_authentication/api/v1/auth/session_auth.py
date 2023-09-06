61e4d39e57a940900fafd8528b44cbee9e855596
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-06 20:21:02 +0200

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,7 +13,7 @@ class SessionAuth(Auth):
     Session authentication class
     """
     user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         """
         Create a Session ID for a user.
