7104fc4a641869ae86c9d0d5e255cf8fbe0142de
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-06 21:25:11 +0200

    style fix

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -5,9 +5,9 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
-from api.v1.views.users import *
+from api.v1.views.index import *  # noqa
+from api.v1.views.users import *  # noqa
 
 User.load_from_file()
 
-from api.v1.views.session_auth import *
+from api.v1.views.session_auth import *  # noqa
