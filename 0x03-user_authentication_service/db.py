0dccd2f982bda6a70fd0c6dd1a45b4c1708e7a89
jdev <20902932+osekojoe@users.noreply.github.com>
2023-09-12 20:01:10 +0200

    db module

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
-        
+
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
